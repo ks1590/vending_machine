377e23a38e9112bd9993bf82913a00e2fc5c069a
kensuke suzuki <35k.0101@gmail.com>
2020-11-26 22:59:56 +0900

    add:redbull,water

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,10 +1,43 @@
+class Drink
+  attr_accessor :name, :price
+
+  def self.cola
+    { price: 120, name: "cola", stock: 5}
+  end
+  
+  def self.redbull
+    { price: 200, name: "redbull", stock: 5}
+  end
+
+  def self.redbull
+    { price: 200, name: "water", stock: 5}
+  end
+end
+
 class VendingMachine
   USABLE_MONEY = [ 10, 50, 100, 500, 1000 ].freeze
+  Drink_Table = ["cola"]
 
   def initialize
-    @drinks = { price: 120, name: :コーラ, stock: 5}
+    @drink = Drink.cola
+    # @drinks = @drink
     @input_money = 0
     @sales = 0
+  end
+
+  def purchase(drink_name)
+    if Drink_Table.include?(drink_name)
+      p true
+    end
+  end
+
+  def select_drink(drink_name)
+    case drink_name
+    when "cola" then
+      @drink = Drink.cola
+    when "redbull" then
+      @drink = Drink.redbull
+    end
   end
 
   def charged_money
@@ -22,8 +55,8 @@ class VendingMachine
   end
   
   def buy
-    if charged_money >= 120 && @drinks[:stock] > 0
-      @drinks[:stock] -= 1
+    if charged_money >= 120 && @drink[:stock] > 0
+      @drink[:stock] -= 1
       @input_money -= 120
       @sales += 120
       return_money
